be26295200ac7ad7e72fbc66073d2e3c91f8943b
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-01-23 18:59:01 +0530

    sort function Array

---
+++ sortFunction.js
@@ -0,0 +1,34 @@
+// const numbers = [1,22,11]
+
+// console.log(numbers.sort())
+// // [ 1, 11, 22 ]
+
+// console.log(numbers)
+// // [ 1, 11, 22 ]
+
+// Using Sort Function
+// const numbers = [1, 22, 11, 8, 45, 2, 1]
+
+// console.log(numbers.sort())
+// // [ 1,  1, 11, 2, 22, 45, 8 ]
+
+// Compare Function
+// function compareNumbers(a, b) {
+//     return a - b;
+//   }
+
+// Ascending Order
+// const numbers = [1, 22, 11, 8, 45, 2, 1]
+// const compareFunction = (a, b) => a-b
+// console.log(numbers.sort(compareFunction))
+// // [ 1,  1, 11, 2, 22, 45, 8 ]
+
+// Descending Order
+// const numbers = [1, 22, 11, 8, 45, 2, 1]
+// const compareFunction = (a, b) => b-a
+// console.log(numbers.sort(compareFunction))
+// // [ 45, 22, 11, 8, 2, 1, 1 ]
+
+var names = ["Like", "Comment", "Save", "Share", "Follow"]
+console.log(names.sort())
+// // [ 'Comment', 'Follow', 'Like', 'Save', 'Share' ]
